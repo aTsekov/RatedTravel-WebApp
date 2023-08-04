ba317f3e5adedfb1091c6313baab8ec3039ae9ec
Antoni Tsekov <102099768+aTsekov@users.noreply.github.com>
2023-08-04 20:29:31 +0200

    Implemented custom login

--- RatedTravel App/RatedTravel.Web/Controllers/UserController.cs
+++ RatedTravel App/RatedTravel.Web/Controllers/UserController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Memory;
 using RatedTravel.Data.DataModels;
 using RatedTravel.Web.ViewModels.User;
+using RaterTravel.Common;
 
 namespace RatedTravel.App.Web.Controllers
 {
@@ -68,9 +69,45 @@ namespace RatedTravel.App.Web.Controllers
                 return RedirectToAction("Index", "Home");
              }
 
+            [HttpGet]
+            public async Task<IActionResult> Login(string? returnUrl = null)
+            {
+                await HttpContext.SignOutAsync(IdentityConstants.ExternalScheme);
+
+                LoginFormModel model = new LoginFormModel()
+                {
+                    ReturnUrl = returnUrl
+                };
+
+                return View(model);
+            }
+
+            [HttpPost]
+            public async Task<IActionResult> Login(LoginFormModel model)
+            {
+                if (!ModelState.IsValid)
+                {
+                    return View(model);
+                }
+
+                var result =
+                    await signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, false);
+
+                if (!result.Succeeded)
+                {
+                    TempData[NotificationMessagesConstants.ErrorMessage] =
+                        "There was an error while logging you in! Please try again later or contact an administrator.";
+
+                    return View(model);
+                }
+
+                return Redirect(model.ReturnUrl ?? "/Home/Index");
+            }
+        }
 
 
 
-    }
+
 }
+
 
